d516607d7af4003df5692c8ba8a78200ef8c2892
Alan Ma <alan@Energymac.local>
2016-11-24 13:57:19 -0500

    Show blog feature added
    Each square now opens up its respective blog post.

---
+++ posts.js
@@ -0,0 +1,37 @@
+var posts = [
+	{
+		id: 5,
+		date: "Nov. 14, 2016",
+		title: "Title",
+		image: "logo.jpg",
+		content: "Hello World!"
+	},
+	{
+		id: 4,
+		date: "Nov. 14, 2016",
+		title: "Title",
+		image: "logo.jpg",
+		content: "Hello World!"
+	},
+	{
+		id: 3,
+		date: "Nov. 14, 2016",
+		title: "Title",
+		image: "logo.jpg",
+		content: "Hello World!"
+	},
+	{
+		id: 2,
+		date: "Nov. 14, 2016",
+		title: "Title",
+		image: "logo.jpg",
+		content: "Hello World!"
+	},
+	{
+		id: 1,
+		date: "Nov. 14, 2016",
+		title: "Title",
+		image: "logo.jpg",
+		content: "Hello World!"
+	}
+]
---
+++ script.js
@@ -0,0 +1,36 @@
+function load_posts() {
+	for (i=0; i<posts.length; i++) {
+        post_id = posts[i].id.toString();
+        $("#posts").append('<div class="post" id="'+post_id+'"><div class="background"></div><div class="content"><h1>'+posts[i].date+'</h1></div></div><div class="blog"><div class="body"><h1>'+posts[i].title+'</h1><h2>'+posts[i].date+'</h2><p>'+posts[i].content+'</p></div><div class="media"><img src="img/'+posts[i].image+'"></div><div class="close">X</div></div>');
+        $("#"+post_id+" .background").css("background", "url(img/"+posts[i].image+")");
+        $("#"+post_id+" .background").css("background-size", "100% 100%");
+	}
+}
+
+function main() {
+	load_posts();
+}
+
+$(document).ready(function() {
+    main();
+});
+
+$(document).ready(function () {
+    $(".blog").on('click', function () {
+        $(this).slideUp(); //toggle the current one
+    });
+    $(".post").on('click', function () {
+        $(".blog").slideUp();
+        var $content = $(this).next(".blog");
+        $content.slideToggle(); //toggle the current one
+    });
+});
+
+$(document).ready(function () {
+    $("#show").on('click', function () {
+        $(".content").slideDown(); //open all
+    });
+    $("#collapse").on('click', function () {
+        $(".content").slideUp(); //close all
+    });
+});
