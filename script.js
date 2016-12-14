f51c1f88d226d07856cefa240428c2e59a8b7aa5
Alan Ma <alan@Energymac.local>
2016-12-14 09:23:47 -0500

    changed order of blogs and posts

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function load_posts() {
 	for (i=0; i<posts.length; i++) {
         post_id = i.toString();
         real_id = posts[i].id.toString();
-        $("#posts").append('<div class="post" id="post'+post_id+'"><div class="background"></div><div class="content"><h1>'+posts[i].date+'</h1></div></div><div data-realid="'+real_id+'" class="blog" id="blog'+post_id+'"><div class="body"><h1>'+posts[i].title+'</h1><h2>'+posts[i].date+'</h2><p>'+posts[i].content+'</p></div><div class="media"><img src="img/'+posts[i].image+'"></div><div class="close">X</div></div>');
+        $("#posts").append('<div data-realid="'+real_id+'" class="blog" id="blog'+post_id+'"><div class="body"><h1>'+posts[i].title+'</h1><h2>'+posts[i].date+'</h2><p>'+posts[i].content+'</p></div><div class="media"><img src="img/'+posts[i].image+'"></div><div class="close">X</div></div><div class="post" id="post'+post_id+'"><div class="background"></div><div class="content"><h1>'+posts[i].date+'</h1></div></div>');
         $("#"+post_id+" .background").css("background", "url(img/"+posts[i].image+")");
         $("#"+post_id+" .background").css("background-size", "100% 100%");
 	}
@@ -50,7 +50,7 @@ $(document).ready(function () {
     });
     $(".post").on('click', function () {
         $(".blog").slideUp();
-        var $content = $(this).next(".blog");
+        var $content = $(this).prev(".blog");
         $content.slideToggle(); //toggle the current one
     });
 });
